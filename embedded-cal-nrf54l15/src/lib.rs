b9b799588ce4c75e67b173f7ae316de0b698ae18
WilliamTakeshi <williamtpereira@gmail.com>
2025-11-27 16:39:21 +0100

    Feat: add defmt_test example

---
+++ embedded-cal-nrf54l15/src/lib.rs
@@ -0,0 +1,21 @@
+#![no_main]
+#![no_std]
+
+use defmt_rtt as _; // global logger
+use panic_probe as _;
+
+#[defmt::panic_handler]
+fn panic() -> ! {
+    cortex_m::asm::udf()
+}
+
+#[cfg(test)]
+#[defmt_test::tests]
+mod unit_tests {
+    use defmt::assert;
+
+    #[test]
+    fn it_works() {
+        assert!(true)
+    }
+}
